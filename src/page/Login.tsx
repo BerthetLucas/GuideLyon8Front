57429769c460bec51f3a4abffaa62a1dd9b3c028
Lucas Berthet <lu.berthet@gmail.com>
2024-04-29 20:03:12 +0200

    failed auth

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,9 +1,95 @@
-import { LoginForm } from "@/components/logic/loginForm";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
 
 export const Login = () => {
+  const formSchema = z.object({
+    user: z.string(),
+    password: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      user: "",
+      password: "",
+    },
+  });
+
+  function hasToken() {
+    const storedToken = localStorage.getItem("authToken");
+    return storedToken !== null;
+  }
+
+  function getToken() {
+    return localStorage.getItem("authToken");
+  }
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        token: "" as string | null,
+      },
+    };
+
+    if (hasToken()) {
+      config.headers["token"] = getToken();
+    }
+
+    axios
+      .post("http://127.0.0.1:8000/api/login", values, config)
+      .then((response) => {
+        localStorage.setItem("authToken", response.data.token);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la requête POST:", error);
+      });
+  }
+
   return (
-    <>
-      <LoginForm />
-    </>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="user"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email Address</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Password" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 };
--- src/page/Registration.tsx
+++ src/page/Registration.tsx
@@ -1,6 +1,36 @@
 import { RegistrationForm } from "@/components/logic/registrationForm";
 
 export const Registration = () => {
+  const formSchema = z.object({
+    email: z.string(),
+    password: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const config = {
+      headers: {
+        "Content-Type": "application/ld+json",
+      },
+    };
+
+    axios
+      .post("http://127.0.0.1:8000/api/users", values, config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la requête POST:", error);
+      });
+  }
+
   return (
     <>
       <section className=" h-screen flex flex-row items-center justify-center">
